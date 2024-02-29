259fb30036767a12402e66dac3a3adbb0d152c05
aqw00 <matheus_tone@hotmail.com>
2024-02-29 11:21:11 +0100

    Added css file

--- resources/views/holiday.blade.php
+++ resources/views/holiday.blade.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_GET['startDate']) && isset($_GET['endDate']) && isset($_GET['submit']))
     {
-        require_once 'C:\xampp\htdocs\PE-app\vendor\autoload.php';
+        require_once 'C:\xampp\htdocs\PracticeEnterpriseHR\vendor\autoload.php';
         $pdfcontent = '<table class="form-data"><thead><tr> </tr></thead><tbody><tr><td>user</td></tr><tr><td>id</td></tr>';
         foreach($_GET as $key =>$value)
         {
@@ -26,43 +26,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        /* Add a black background color to the top navigation */
-        .topnav{background-color: #333; overflow: hidden;}
-
-        /* Style the links inside the navigation bar */
-        .topnav a {
-        float: left;
-        color: #f2f2f2;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-        }
-
-        /* Change the color of links on hover */
-        .topnav a:hover {
-        background-color: #ddd;
-        color: black;
-        }
-
-        /* Add a color to the active/current link */
-        .topnav a.active {
-        background-color: #04AA6D;
-        color: white;
-        }
-    </style>
     <title>holiday</title>
+    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
 </head>
-<header>
-<div class="topnav">
-  <a href="{{ route('home') }}">Home</a>
-  <a class="active" href="{{ route('holiday') }}">Holiday</a>
-  <a  href="{{ route('man') }}">ManagerPage</a>
-  <a href="#about">About</a>
-</div>
-</header>
 <body>
+    <header>
+    <div class="topnav">
+    <a href="{{ route('home') }}">Home</a>
+    <a class="active" href="{{ route('holiday') }}">Holiday</a>
+    <a  href="{{ route('man') }}">ManagerPage</a>
+    <a href="#about">About</a>
+    </div>
+    </header>
     <h1>Welcome to Holiday Page!</h1>
     <table class="form-data">
      <form action="/holiday" method="get">  
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,43 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        /* Add a black background color to the top navigation */
-        .topnav{background-color: #333; overflow: hidden;}
-
-        /* Style the links inside the navigation bar */
-        .topnav a {
-        float: left;
-        color: #f2f2f2;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-        }
-
-        /* Change the color of links on hover */
-        .topnav a:hover {
-        background-color: #ddd;
-        color: black;
-        }
-
-        /* Add a color to the active/current link */
-        .topnav a.active {
-        background-color: #04AA6D;
-        color: white;
-        }
-    </style>
     <title>home</title>
+    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
 </head>
-<header>
-<div class="topnav">
-  <a class="active" href="{{ url('/home') }}">Home</a>
-  <a href="{{ url('/holiday') }}">Holiday</a>
-  <a  href="{{ url('/man') }}">ManagerPage</a>
-  <a href="#about">About</a>
-</div>
-</header>
 <body>
+<header>
+    <div class="topnav">
+    <a class="active" href="{{ url('/home') }}">Home</a>
+    <a href="{{ url('/holiday') }}">Holiday</a>
+    <a  href="{{ url('/man') }}">ManagerPage</a>
+    <a href="#about">About</a>
+    </div>
+    </header>
     <h1>Welcome Home!</h1>
 </body>
 </html>
